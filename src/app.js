7ea18eb225744a2770201cff9344455e4a17a520
willson <willsoncatacora1@gmail.com>
2024-03-07 21:56:13 -0400

    Se documento la API-REST con swagger-ui-express.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const bodyPaser = require("body-parser");
+const apicache = require("apicache");
 const v1Router = require("./routers/v1/workouts.router");
+const { swaggerDocs: V1SwaggerDocs } = require("./routers/swagger");
+
 const env = require("dotenv");
 env.config();
 // PUERTO
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(bodyPaser.json());
+const cache = apicache.middleware;
 
 app.get("/", (req, res) => {
   res.send("API REST con CRUD.");
 });
 
+app.use(bodyPaser.json());
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1Router);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto : ${port}`);
+  V1SwaggerDocs(app, port);
 });
--- src/routers/v1/workouts.router.js
+++ src/routers/v1/workouts.router.js
@@ -1,11 +1,241 @@
-const express = require('express')
-const workoutController = require('../../controllers/workouts.controllers')
-const router = express.Router()
+const express = require("express");
+const workoutController = require("../../controllers/workouts.controllers");
+const router = express.Router();
 
-router.get('/', workoutController.getAllWorkouts)
-router.get('/:id', workoutController.getOneWorkout)
-router.post('/', workoutController.createWorkout)
-router.patch('/:id', workoutController.updateWorkout)
-router.delete('/:id', workoutController.deleteWorkout)
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
+router.get("/", workoutController.getAllWorkouts);
 
-module.exports = router
+/**
+ * @openapi
+ * /api/v1/workouts/{id}:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+router.get("/:id", workoutController.getOneWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   post:
+ *     tags:
+ *       - Workouts
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Workout"
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Created
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Invalid request body"
+ */
+router.post("/", workoutController.createWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts/{id}:
+ *   patch:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the workout
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Workout"
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Invalid request body"
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+router.patch("/:id", workoutController.updateWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts/{id}:
+ *   delete:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the workout to delete
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+router.delete("/:id", workoutController.deleteWorkout);
+
+module.exports = router;
